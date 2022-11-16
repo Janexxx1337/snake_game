252ab7b24d23899012f088b873b5d958047be642
Your Name <yourgmail address>
2022-11-16 09:56:45 +0300

    End 1-3

--- snake.py
+++ snake.py
@@ -4,16 +4,10 @@ x_positions = [0, -20, -40]
 segments = []
 
 
-
-
 class Snake:
-
-
     def __init__(self):
         self.segments = []
         self.create_snake()
-
-
 
     def create_snake(self):
         for i in range(0, 3):

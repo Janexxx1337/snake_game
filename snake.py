0b81b9ed374525010a26cd0388f3e44be1110337
Your Name <yourgmail address>
2022-11-16 10:02:38 +0300

    End 1-3

--- snake.py
+++ snake.py
@@ -1,7 +1,7 @@
 from turtle import Screen, Turtle
 x_positions = [0, -20, -40]
+move_distance = 20
 
-segments = []
 
 
 class Snake:
@@ -17,10 +17,10 @@ class Snake:
             turtle.goto(y=0, x=x_positions[i])
             self.segments.append(turtle)
 
-    def run(self):
+    def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
 
-        self.segments[0].forward(20)
+        self.segments[0].forward(move_distance)

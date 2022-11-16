42521e19dbda99db7a0cdf05bb359aafc3f1a4bb
Your Name <yourgmail address>
2022-11-16 10:42:37 +0300

    Snake_move

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ screen.tracer(0)
 x_positions = [0, -20, -40]
 
 snake = Snake()
-
-
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
 
 
--- snake.py
+++ snake.py
@@ -1,26 +1,46 @@
-from turtle import Screen, Turtle
-x_positions = [0, -20, -40]
-move_distance = 20
-
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
+
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
-        for i in range(0, 3):
-            turtle = Turtle(shape='square')
-            turtle.color('white')
-            turtle.penup()
-            turtle.goto(y=0, x=x_positions[i])
-            self.segments.append(turtle)
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
-        self.segments[0].forward(move_distance)
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)

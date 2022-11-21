f0344f8c750c924f253e5e0cbd8ce64819088912
Your Name <yourgmail address>
2022-11-21 13:02:59 +0300

    Done don

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,18 +10,16 @@ screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0)
 
-
 x_positions = [0, -20, -40]
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(snake.up, 'Up')
 screen.onkey(snake.down, 'Down')
 screen.onkey(snake.left, 'Left')
 screen.onkey(snake.right, 'Right')
-
-
-
 
 game_is_on = True
 
@@ -28,6 +28,22 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increaseScore()
+
+    # detected collision
+
+    if snake.head.xcor() > 285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < - 285:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
-

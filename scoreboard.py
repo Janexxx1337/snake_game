9f3a05591ab8268d1490cc9271936f7056d47a82
Your Name <yourgmail address>
2022-11-28 14:11:17 +0300

    Add Global_score

--- scoreboard.py
+++ scoreboard.py
@@ -1,28 +1,33 @@
 from turtle import Turtle
-ALIGNMENT = "center"
-FONT = ('Arial',22,'normal')
 
+ALIGNMENT = "center"
+FONT = ('Arial', 22, 'normal')
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.goto(x=0, y=270)
+        with open('data.txt') as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.color('white')
         self.score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score {self.score}: ", False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(f'{self.high_score}')
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
-
+        self.score = 0
 
     def increaseScore(self):
         self.score += 1
-        self.clear()
+
         self.update_scoreboard()
